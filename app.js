a8510850473df7550497914b2f975986bb6c1a02
Phil <182430206+Fico27@users.noreply.github.com>
2025-09-20 12:23:33 +0200

    installed needed packages and the basic outline for app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("node:path");
+const session = require("express-session");
+const passport = require("passport");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(3000, (errpr) => {
+  if (error) {
+    throw error;
+  }
+  console.log("Listening on port 3000!");
+});
